75a40eed7cb3c53b6fac60d362f6bb81734d850f
Gwenaëlle Gustin <gwenaelle.gustin@students.hevs.ch>
2022-03-05 15:21:40 +0100

    firestore link

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,52 @@ import '../styles/App.css';
 import Form from "./Form";
 import Login from "./Login";
 
+import { onSnapshot, collection } from "firebase/firestore";
+import {useEffect} from "react";
+import {useState} from "react";
+import db from "../config/firebase"
+
 function App() {
-  const isLogged = true;
-  let componentToDisplay;
+    const isLogged = true;
+    let componentToDisplay;
 
-  if(!isLogged){
-      componentToDisplay = <Login/>
-  } else {
-      componentToDisplay = <Form/>
-  }
+    // test Firestore
+    const [users, setUsers] = useState([]);
+    useEffect(
+        () =>
+        onSnapshot(collection(db, "users"),(snapshot) =>
+            setUsers(snapshot.docs.map((doc) => doc.data()))
+            ),
+        []
+    );
 
-  return (
-    <div className="App">
-      <header className="App-header">
-          {componentToDisplay}
-      </header>
-    </div>
-  );
+    if (!isLogged) {
+        componentToDisplay = <Login/>
+    } else {
+        componentToDisplay = <Form/>
+    }
+
+    return (
+        <div className="App">
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>
+                        <p>{user.name}</p>
+                    </li>
+                ))}
+            </ul>
+            <header className="App-header">
+                {componentToDisplay}
+            </header>
+        </div>
+    );
 }
-
+// Get a list of cities from your database
+/*function getUsers(db) {
+    const usersCol = collection(db, 'users');
+    const userSnapshot = getDocs(usersCol);
+    const userList = userSnapshot.docs.map(doc => doc.data());
+    console.log(userList)
+    //return userList;
+}*/
 export default App;
