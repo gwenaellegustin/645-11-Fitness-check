fd8a3fc074653e59be54da3e876a3989af64d81e
antonymarques <antony.marques@hotmail.ch>
2022-04-17 11:27:12 +0200

    refactor: Add comment on component

--- src/components/form/CategoryContainer.js
+++ src/components/form/CategoryContainer.js
@@ -1,6 +1,16 @@
 import {QuestionContainer} from "./QuestionContainer";
 import {FormGroup} from "reactstrap";
 
+/**
+ * Component concerning a specific category
+ *
+ * @param category linked to this container
+ * @param questions linked to this category
+ * @param isDisplayMode for edit or read-only mode
+ * @param completedAnswersId for read-only mode in order to select answered answers
+ *
+ * @author Antony
+ */
 export function CategoryContainer({category, questions, isDisplayMode, completedAnswersId}) {
 
     //Return a question container with the question object (containing the label, answers, isUniqueAnswer)
