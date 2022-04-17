4bc4b01006af68d3d983749e4b104270516b6499
antonymarques <antony.marques@hotmail.ch>
2022-04-17 10:26:24 +0200

    refactor: CategoryContainer.js clean

--- src/components/form/CategoryContainer.js
+++ src/components/form/CategoryContainer.js
@@ -2,20 +2,20 @@ import {QuestionContainer} from "./QuestionContainer";
 import {FormGroup} from "reactstrap";
 import {useEffect} from "react";
 
-export function CategoryContainer({category, questions, isDisplayMode, completedAnswersId}){
+export function CategoryContainer({category, questions, isDisplayMode, completedAnswersId}) {
 
     useEffect(() => {
         //Order question alphabetically
-        questions.sort((a,b) => a.label.localeCompare(b.label));
+        questions.sort((a, b) => a.label.localeCompare(b.label));
     }, [questions])
-    
 
     //Return a question container with the question object (containing the label, answers, isUniqueAnswer)
     return (
         <FormGroup>
-            <legend>{category.label}</legend >
+            <legend>{category.label}</legend>
             {questions.map(question => (
-                    <QuestionContainer key={question.id} question={question} isDisplayMode={isDisplayMode} completedAnswersId={completedAnswersId}/>
+                <QuestionContainer key={question.id} question={question} isDisplayMode={isDisplayMode}
+                                   completedAnswersId={completedAnswersId}/>
             ))}
         </FormGroup>
     )
