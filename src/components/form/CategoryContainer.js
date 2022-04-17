119a8d589ea4c67ea710074b396f3ef51a7b7767
antonymarques <antony.marques@hotmail.ch>
2022-04-17 11:19:43 +0200

    fix: Order questions alphabetically by label

--- src/components/form/CategoryContainer.js
+++ src/components/form/CategoryContainer.js
@@ -1,19 +1,15 @@
 import {QuestionContainer} from "./QuestionContainer";
 import {FormGroup} from "reactstrap";
-import {useEffect} from "react";
 
 export function CategoryContainer({category, questions, isDisplayMode, completedAnswersId}) {
-
-    useEffect(() => {
-        //Order question alphabetically
-        questions.sort((a, b) => a.label.localeCompare(b.label));
-    }, [questions])
 
     //Return a question container with the question object (containing the label, answers, isUniqueAnswer)
     return (
         <FormGroup>
             <legend>{category.label}</legend>
-            {questions.map(question => (
+            {questions
+                .sort((a, b) => a.label.localeCompare(b.label)) //Sort alphabetically by label
+                .map(question => (
                 <QuestionContainer key={question.id} question={question} isDisplayMode={isDisplayMode}
                                    completedAnswersId={completedAnswersId}/>
             ))}
